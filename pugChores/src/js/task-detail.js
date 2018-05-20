c73b772d6642e79df22584be3d464d34f7ea1270
Bum Mook Oh <bmo5@uw.edu>
2018-05-20 00:15:49 -0700

    parent can create reward, child can redeem reward

--- pugChores/src/js/task-detail.js
+++ pugChores/src/js/task-detail.js
@@ -3,23 +3,63 @@ var database = firebase.database();
 var url_string = window.location.href
 var url = new URL(url_string);
 var taskUID = url.searchParams.get("taskUID");
-let familyUID = "-LCvzr3C6CAO6qIJiaid"; //sessionStorage.getItem("familyUID");
+var rewardUID = url.searchParams.get("rewardUID");
 
-let taskName = document.getElementById("taskName");
+let familyUID = "-LCw5ow5u64CdtprojEp"; //sessionStorage.getItem("familyUID"); TODO: change
+let userUID = "-LCwAQIZMo3tGGDwZWgr"; //sessionStorage.getItem("userUID"); TODO: change
+let userPoints = 20; //sessionStorage.getItem("userPoints"); TODO: change
+
+let name = document.getElementById("name");
 let value = document.getElementById("value");
 let note = document.getElementById("note");
+if (taskUID != null) {
+    database.ref("family/" + familyUID + "/tasks/" + taskUID)
+        .once('value')
+        .then(function (snapshot) {
+            let data = snapshot.val();
+            name.innerText = data.name;
+            value.innerText = data.value;
+            note.innerText = data.description;
+        });
+} else {
+    database.ref("family/" + familyUID + "/rewards/" + rewardUID)
+        .once('value')
+        .then(function (snapshot) {
+            let data = snapshot.val();
+            name.innerText = data.name;
+            value.innerText = data.value;
+            note.innerText = data.description;
+        });
+}
 
-database.ref("family/" + familyUID + "/tasks/" + taskUID)
-    .once('value')
-    .then(function (snapshot) {
-        data = snapshot.val();
-        taskName.innerText = data.name;
-        value.innerText = data.value;
-        note.innerText = data.description;
-    });
 
-function onDeletePress() {
+function deleteTask() {
     database.ref("family/" + familyUID + "/tasks/" + taskUID)
         .remove();
     window.location.replace("parent-tasks.html");
+}
+
+function deleteReward() {
+    database.ref("family/" + familyUID + "/rewards/" + rewardUID)
+        .remove();
+    window.location.replace("parent-rewards.html");
+}
+
+function redeemReward() {
+    var completedRewardObject = {
+        name: name.textContent,
+        value: value.textContent,
+        description: note.textContent,
+        completedBy: userUID
+    };
+    database.ref("family/" + familyUID + "/rewards/" + rewardUID)
+        .remove();
+    database.ref("family/" + familyUID + "/completed").push(completedRewardObject);
+    let deductedPoints = userPoints - parseInt(value.textContent);
+    database.ref("family/" + familyUID + "/familyUsers/" + userUID)
+        .update({
+            "points": deductedPoints
+        });
+    sessionStorage.setItem("userPoints", deductedPoints);
+    window.location.replace("child-rewards.html");
 }
