c8862da49cad755b0c0fd7ac461131debf188237
Wesley Wei <wesley631w@gmail.com>
2018-05-23 01:12:57 -0700

    Working edit profile now.

--- src/js/edit-profile.js
+++ src/js/edit-profile.js
@@ -8,7 +8,7 @@ let userUID = sessionStorage.getItem("userUID");
 let gravatar = document.getElementById("gravatar");
 let name = document.getElementById("name");
 let email = document.getElementById("email");
-var oldEmail = "";
+var oldEmailHash = "";
 
 database.ref("family/" + familyUID + "/familyUsers/" + userUID)
         .once('value')
@@ -16,7 +16,7 @@ database.ref("family/" + familyUID + "/familyUsers/" + userUID)
             let data = snapshot.val();
             name.value = data.name;
             email.value = data.email;
-            oldEmail = data.emailHash;
+            oldEmailHash = data.emailHash;
         })
         .then(() => {
             M.updateTextFields();
@@ -26,35 +26,18 @@ function goBack(){
     window.history.back();
 }
 
-// var doneSave = save();
-
-// doneSave.done(function() {
-//     removeOldUser();
-//     goBack();
-// });
-
-// $.when(doneSave).done(function() {
-//     removeOldUser();
-//     goBack();
-// });
-
-var save = function () {
-    // var user = firebase.auth().currentUser;
-    // user.updateEmail(email.value).then(function() {
-    //     console.log("success");
-    // }).catch(function(error) {
-    //     console.log(error);
-    // });
-    var r = $.Deferred();
-    console.log(oldEmail);
+function save () {
+    var user = firebase.auth().currentUser;
+    user.updateEmail(email.value).then(function() {
+        console.log("success");
+    }).catch(function(error) {
+        console.log(error);
+    });
     var userRef = database.ref("users");
-    userRef.child(oldEmail).once("value")
+    userRef.child(oldEmailHash).once("value")
         .then(function(snap) {
-            console.log(snap.key);
             var data = snap.val();
-            console.log(data);
             let newEmailHash = md5(email.value);
-            console.log(newEmailHash);
             database.ref("users/" + newEmailHash).set(data);
         })
         .then(
@@ -65,13 +48,16 @@ var save = function () {
                     emailHash : md5(email.value)
                 })
         );
-    return r.resolve();
+    setTimeout(function() {
+        removeOldUser();       
+    }, 1000);
+    setTimeout(function() {
+        goBack();    
+    }, 1200);
 }
 
-var removeOldUser = function () {
-    var oldReference = database.ref("users/" + oldEmail);
+function removeOldUser () {
+    var oldReference = database.ref("users/" + oldEmailHash);
     oldReference.remove();
-    goBack();
 }
 
-save().done(removeOldUser);
