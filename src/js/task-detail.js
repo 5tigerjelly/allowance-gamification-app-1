a4791fb773658407430c1e32bbdf92c609ba2e89
Bum Mook Oh <bmo5@uw.edu>
2018-05-24 11:05:38 -0700

    not sure

--- src/js/task-detail.js
+++ src/js/task-detail.js
@@ -5,13 +5,14 @@ var url = new URL(url_string);
 var taskUID = url.searchParams.get("taskUID");
 var rewardUID = url.searchParams.get("rewardUID");
 
-let familyUID = sessionStorage.getItem("familyUID"); 
+let familyUID = sessionStorage.getItem("familyUID");
 let userUID = sessionStorage.getItem("userUID");
 let userPoints = sessionStorage.getItem("currPoints");
 
 let name = document.getElementById("name");
 let value = document.getElementById("value");
 let note = document.getElementById("note");
+
 if (taskUID != null) {
     database.ref("family/" + familyUID + "/tasks/" + taskUID)
         .once('value')
@@ -46,25 +47,30 @@ function deleteReward() {
 }
 
 function redeemReward() {
-    database.ref("family/" + familyUID + "/rewards/" + rewardUID)
-        .update({
-            "completedBy": userUID,
-            "status" : "claimed"
-        });
-    let deductedPoints = userPoints - parseInt(value.textContent);
-    database.ref("family/" + familyUID + "/familyUsers/" + userUID)
-        .update({
-            "points": deductedPoints
-        });
-    sessionStorage.setItem("currPoints", deductedPoints);
-    window.location.replace("./child-rewards.html");
+    if (false) {
+
+    } else {
+        database.ref("family/" + familyUID + "/rewards/" + rewardUID)
+            .update({
+                "completedBy": userUID,
+                "status": "claimed"
+            });
+        let deductedPoints = userPoints - parseInt(value.textContent);
+        database.ref("family/" + familyUID + "/familyUsers/" + userUID)
+            .update({
+                "points": deductedPoints
+            });
+        sessionStorage.setItem("currPoints", deductedPoints);
+        window.location.replace("./child-rewards.html");
+    }
+
 }
 
 function accpetTask() {
     database.ref("family/" + familyUID + "/tasks/" + taskUID)
         .update({
             "inProgressBy": userUID,
-            "status" : "inProgress"
+            "status": "inProgress"
         });
-    window.location.replace("./inProgress.html?taskUID="+taskUID);
+    window.location.replace("./inProgress.html?taskUID=" + taskUID);
 }
