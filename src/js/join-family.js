a2a1e21d15579c1bf6d233a00d38f18192f0c345
Wesley Wei <wesley631w@gmail.com>
2018-05-26 13:12:44 -0700

    Show error when trying to join family if password is incorrect.

--- src/js/join-family.js
+++ src/js/join-family.js
@@ -34,9 +34,14 @@ var database = firebase.database();
 function verifyPassword() {
     var correctPassword = false;
     var familyName = document.getElementById("familyName").value;
-    var password = document.getElementById("password").value;
+    var passwordVal = document.getElementById("password").value;
     var joinBtn = document.getElementById("join-btn");
     var joinBtnTxt = document.getElementById("join-btn-txt");
+    var familyNameElem = document.getElementById("familyName");
+    var passwordElem = document.getElementById("password");
+
+    familyNameElem.classList.remove("invalid");
+    passwordElem.classList.remove("invalid");
     
     var ref = database.ref("family")
         .once("value")
@@ -45,14 +50,47 @@ function verifyPassword() {
                 let child = familyRef.child(family.key);
                 let tempFamilyName = child.val().name;
                 let tempFamilyPassword = child.val().password;
-                if (tempFamilyName === familyName && password === tempFamilyPassword) {
+                // console.log(tempFamilyName);
+                // console.log(familyName);
+                if (tempFamilyName === familyName && passwordVal !== tempFamilyPassword) {
+                    passwordElem.classList.add("invalid");
+                } else if (tempFamilyName === familyName && passwordVal === tempFamilyPassword) {
                     correctPassword = true;
                     joinBtn.classList.remove("hidden");
                     joinBtnTxt.classList.add(family.key);
                     sessionStorage.setItem("familyName", familyName);
+                } 
+            });
+        })
+}
+
+// var containsFamily = false;
+
+function checkFamilyExists() {
+    var containsFamily = false;
+    var familyName = document.getElementById("familyName").value;
+    var familyNameElem = document.getElementById("familyName");
+
+    familyNameElem.classList.remove("invalid");
+    
+    database.ref("family")
+        .once("value")
+        .then(function (familyRef) {
+            familyRef.forEach(function (family) {
+                let child = familyRef.child(family.key);
+                let tempFamilyName = child.val().name;
+                let tempFamilyPassword = child.val().password;
+                if (tempFamilyName === familyName) {
+                    containsFamily = true;
                 }
             });
         })
+        .finally(() => {
+            console.log(containsFamily);
+            if (!containsFamily) {
+                familyNameElem.classList.add("invalid");
+            }
+        });
 }
 
 function onButtonPress() {
