44966de9cde30fa7bf64bd934a4797326ddfcb86
Bum Mook Oh <bmo5@uw.edu>
2018-05-30 00:06:21 -0700

    created seperate js for creating rewards

--- src/js/child-reward.js
+++ src/js/child-reward.js
@@ -62,20 +62,3 @@ function createTaskItem(data, taskUID) {
     return a;
 }
 
-function createReward() {
-    let rewardName = document.getElementById("rewardName").value;
-    let value = document.getElementById("value").value;
-    let note = document.getElementById("note").value;
-
-    let familyUID = sessionStorage.getItem("familyUID");
-
-    var rewardObject = {
-        name: rewardName,
-        value: value,
-        description: note,
-        status : "avaliable"
-    };
-
-    database.ref("family/" + familyUID + "/rewards").push(rewardObject);
-    window.location.replace("./parent-rewards.html");
-}
