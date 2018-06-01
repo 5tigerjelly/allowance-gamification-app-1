913512e0044eb17cdcc8a766a6d184e10a3d9061
Wesley Wei <wesley631w@gmail.com>
2018-05-31 17:19:15 -0700

    Fixed positioning of header family title and vert button dropdown appearance.

--- src/js/main.js
+++ src/js/main.js
@@ -11,16 +11,3 @@ function logout(){
     });
     window.location.replace("./index.html");
 }
-
-function changeBgColor() {
-    // let bodyElem = document.querySelectorAll("body *");
-    // if (bodyElem.classList.contains("dropdownBgColor")) {
-    //     bodyElem.classList.remove("dropdownBgColor");
-    // } else {
-    //     bodyElem.classList.add("dropdownBgColor");
-    // }
-    var els = document.getElementsByTagName("*");
-    for(var i = 0, all = els.length; i < all; i++){   
-         els[i].classList.add('dropdownBgColor');
-     }
-}
