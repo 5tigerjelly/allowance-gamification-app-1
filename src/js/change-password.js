8556c2beff3474f2460afa9f19cb8119d9a4b893
Wesley Wei <wesley631w@gmail.com>
2018-06-06 12:34:24 -0700

    Made changepassword icons clickable to view password.

--- src/js/change-password.js
+++ src/js/change-password.js
@@ -1,29 +1,65 @@
-function toggleCurrPasswordIcon() {
-    let visibilityIcon = document.getElementById("currPasswordIcon");
-    if (visibilityIcon.classList.contains("hidden")) {
-        visibilityIcon.classList.remove("hidden");
-    } else {
-        visibilityIcon.classList.add("hidden");
-    }
-}
+// function toggleCurrPasswordIcon() {
+//     let visibilityIcon = document.getElementById("currPasswordIcon");
+//     if (visibilityIcon.classList.contains("hidden")) {
+//         visibilityIcon.classList.remove("hidden");
+//     } else {
+//         visibilityIcon.classList.add("hidden");
+//     }
+// }
 
-function toggleNewPasswordIcon() {
-    let visibilityIcon = document.getElementById("newPasswordIcon");
-    if (visibilityIcon.classList.contains("hidden")) {
-        visibilityIcon.classList.remove("hidden");
-    } else {
-        visibilityIcon.classList.add("hidden");
-    }
-}
+// function toggleNewPasswordIcon() {
+//     let visibilityIcon = document.getElementById("newPasswordIcon");
+//     if (visibilityIcon.classList.contains("hidden")) {
+//         visibilityIcon.classList.remove("hidden");
+//     } else {
+//         visibilityIcon.classList.add("hidden");
+//     }
+// }
 
-function toggleConfirmNewPassIcon() {
-    let visibilityIcon = document.getElementById("confirmNewPassIcon");
-    if (visibilityIcon.classList.contains("hidden")) {
-        visibilityIcon.classList.remove("hidden");
+// function toggleConfirmNewPassIcon() {
+//     let visibilityIcon = document.getElementById("confirmNewPassIcon");
+//     if (visibilityIcon.classList.contains("hidden")) {
+//         visibilityIcon.classList.remove("hidden");
+//     } else {
+//         visibilityIcon.classList.add("hidden");
+//     }
+// }
+
+let currPasswordIcon = document.getElementById("currPasswordIcon");
+let currPasswordElem = document.getElementById("currPassword");
+currPasswordIcon.addEventListener("click", function() {
+    if (currPasswordIcon.innerText == "visibility_off") {
+        currPasswordIcon.innerText = "visibility";
+        currPasswordElem.type = "text";
     } else {
-        visibilityIcon.classList.add("hidden");
+        currPasswordIcon.innerText = "visibility_off";
+        currPasswordElem.type = "password";
     }
-}
+});
+
+let newPasswordIcon = document.getElementById("newPasswordIcon");
+let newPasswordElem = document.getElementById("newPassword");
+newPasswordIcon.addEventListener("click", function() {
+    if (newPasswordIcon.innerText == "visibility_off") {
+        newPasswordIcon.innerText = "visibility";
+        newPasswordElem.type = "text";
+    } else {
+        newPasswordIcon.innerText = "visibility_off";
+        newPasswordElem.type = "password";
+    }
+});
+
+let confirmNewPassIcon = document.getElementById("confirmNewPassIcon");
+let confirmPassElem = document.getElementById("confirmNewPassword");
+confirmNewPassIcon.addEventListener("click", function() {
+    if (confirmNewPassIcon.innerText == "visibility_off") {
+        confirmNewPassIcon.innerText = "visibility";
+        confirmPassElem.type = "text";
+    } else {
+        confirmNewPassIcon.innerText = "visibility_off";
+        confirmPassElem.type = "password";
+    }
+});
 
 function goBack() {
     window.history.back();
@@ -53,15 +89,16 @@ function save() {
         }).catch(function (error) {
             console.log("Error: " + error);
             passHelperText.setAttribute("data-error", error);
-        firebase.auth().onAuthStateChanged(function (user) {
-            if (user) {
-                user.updatePassword(confirmNewPassword).then(function () {
-                    sessionStorage.setItem("userPasswordHash", md5(confirmNewPassword));
-                    goBack();
-                }).catch(function (error) {
-                    passHelperText.setAttribute("data-error", error);
-                });
-            }
+            firebase.auth().onAuthStateChanged(function (user) {
+                if (user) {
+                    user.updatePassword(confirmNewPassword).then(function () {
+                        sessionStorage.setItem("userPasswordHash", md5(confirmNewPassword));
+                        goBack();
+                    }).catch(function (error) {
+                        passHelperText.setAttribute("data-error", error);
+                    });
+                }
+            });
         });
     }
 }
